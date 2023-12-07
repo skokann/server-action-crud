3ff28bc17af03a79e894f9acfc78a033386b3967
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-12-07 22:18:03 +0100

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,7 @@
 export default async function page({ id }: { id: string }) {
   return (
     <div className="font-mono">
-      <h1>CRUD APP WITH NEXT.JS SERVER ACTIONS</h1>
-      <p>Simple nextjs app with server actions.</p>
-      <form action="" className="">
-        <label htmlFor="text" className="block">
-          Note
-        </label>
-        <textarea
-          name="text"
-          id="text"
-          cols={30}
-          rows={10}
-          className="border border-black p-2"
-        />
-        <button type="submit" className="p-2 bg-blue-400 block">
-          Submit
-        </button>
-      </form>
+      <h1>NOT WORKING YEAT!</h1>
     </div>
   );
 }
